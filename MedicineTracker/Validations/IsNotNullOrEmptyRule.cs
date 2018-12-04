64621b8d16c992e4438ce6ad506207d0cbede8bc
Eric Groseclos <ericgroseclos@hotmail.com>
2018-12-04 11:42:11 +0100

    Removed getter and setter methods for the Rule class which implements the IValidationRule.

--- MedicineTracker/Validations/IsNotNullOrEmptyRule.cs
+++ MedicineTracker/Validations/IsNotNullOrEmptyRule.cs
@@ -5,8 +5,6 @@ namespace MedicineTracker.Validation
 {
     public class IsNotNullOrEmptyRule<T> : IValidationRule<T>
     {
-        public string ValidationMessage { get; set; }
-
 
         public bool Check(T value)
         {
